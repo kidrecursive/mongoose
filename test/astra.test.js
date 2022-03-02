b682999fc49bdd796310d6254d9b07bc26cbccde
CRW <chris.wilhite@datastax.com>
2022-03-02 14:19:09 -0500

    sync

--- test/astra.test.js
+++ test/astra.test.js
@@ -2,14 +2,18 @@
 
 require('dotenv').config();
 const mongoose = require('../lib');
+const uniqueValidator = require('mongoose-unique-validator');
 // const assert = require('assert');
 
 const BASE_URL = `https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com`;
 
 const cartSchema = new mongoose.Schema({
   name: String,
+  cartName: { type: String, lowercase: true, unique: true, index: true },
   products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }]
 });
+
+cartSchema.plugin(uniqueValidator, { message: 'is already taken.' });
 
 const productSchema = new mongoose.Schema({
   name: String,
@@ -23,7 +27,7 @@ describe('astra:', () => {
   it('should leverage astradb', async() => {
     await mongoose.connect(BASE_URL, {
       dbName: process.env.ASTRA_DB_KEYSPACE,
-      astraApplicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+      astraApplicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN
     });
 
     const product1 = new Product({ name: 'Product 1', price: 10 });
@@ -34,17 +38,26 @@ describe('astra:', () => {
 
     const cart = new Cart({
       name: 'My Cart',
+      cartName: 'wewson',
       products: [product1._id, product2._id]
     });
     await cart.save();
 
-    const res = await Cart.findOne({ name: { $eq: 'My Cart' } })
-      .populate('products')
-      .exec();
+    // const count = await Cart.countDocuments({ name: { $eq: 'My Cart' } });
+    // console.log(count);
 
-    console.log(res);
+    // const res = await Cart.find({ name: { $eq: 'My Cart' } }).exec();
+    // console.log(res);
 
-    await mongoose.connection.db.dropCollection('carts');
-    await mongoose.connection.db.dropCollection('products');
+    // const res = await Cart.findOne({ name: { $eq: 'My Cart' } })
+    //   .populate('products')
+    //   .exec();
+    // console.log(res);
+    const count = await Cart.find({
+      name: { $eq: 'My Cart' }
+    }).countDocuments();
+    console.log(count);
+    // await mongoose.connection.db.dropCollection('carts');
+    // await mongoose.connection.db.dropCollection('products');
   });
 });
